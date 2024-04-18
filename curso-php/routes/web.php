dc9d475f07aa844ca45b6089a5964ff2556a3c29
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-04-18 11:25:41 -0400

    entendo cada vez mais o blade.php

--- curso-php/routes/web.php
+++ curso-php/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $nome = "Lucas";
+    $idade = 18;
+
+    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'profissao' => "Programador"]);
 });
