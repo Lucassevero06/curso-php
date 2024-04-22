104ceaa58c923d934d193b9e89edbfe5e29db83b
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-04-22 13:54:46 -0400

    tem algum erro ainda não descoberto

--- curso-php/resources/views/welcome.blade.php
+++ curso-php/resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
                     <p class="card-date">xx/xx/xxxx</p>
                     <h5 class="card-title">{{ $event->title }}</h5>
                     <p class="card-participants">x participantes</p>
-                    <a href="/events/{{ $event->$id }}" class="btn btn-primary">Saber mais</a>
+                    <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
                 </div>
             </div>
             @endforeach
